20cb3b1c577fbff13896a064b11dae976fd270e3
Mark Mckessock <mark.mckessock@gmail.com>
2023-02-16 14:03:05 -0500

    Remove extra external dns config

---
+++ terraform/cloudflare/dns_markmckessock_com.tf
@@ -0,0 +1,79 @@
+data "cloudflare_zones" "public_domain" {
+  filter {
+    name = "markmckessock.com"
+  }
+}
+
+resource "cloudflare_record" "host_rpi" {
+  name    = "rpi-ha.infra"
+  zone_id = lookup(data.cloudflare_zones.public_domain.zones[0], "id")
+  value   = "10.0.60.20"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
+
+
+resource "cloudflare_record" "host_kari" {
+  name    = "kari.saturn.kube"
+  zone_id = lookup(data.cloudflare_zones.public_domain.zones[0], "id")
+  value   = "10.0.70.2"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "host_titan" {
+  name    = "titan.saturn.kube"
+  zone_id = lookup(data.cloudflare_zones.public_domain.zones[0], "id")
+  value   = "10.0.70.3"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "shost_phoebe" {
+  name    = "phoebe.saturn.kube"
+  zone_id = lookup(data.cloudflare_zones.public_domain.zones[0], "id")
+  value   = "10.0.70.4"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "host_hyperion" {
+  name    = "hyperion.saturn.kube"
+  zone_id = lookup(data.cloudflare_zones.public_domain.zones[0], "id")
+  value   = "10.0.70.5"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "host_janus" {
+  name    = "janus.saturn.kube"
+  zone_id = lookup(data.cloudflare_zones.public_domain.zones[0], "id")
+  value   = "10.0.70.7"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
+
+
+resource "cloudflare_record" "host_granite" {
+  name    = "granite"
+  zone_id = lookup(data.cloudflare_zones.public_domain.zones[0], "id")
+  value   = "10.0.70.8"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "mosquitto" {
+  name    = "mosquitto"
+  zone_id = lookup(data.cloudflare_zones.public_domain.zones[0], "id")
+  value   = "10.0.70.131"
+  proxied = false
+  type    = "A"
+  ttl     = 1
+}
